72490aff84c27fd72ef723b438933678d728b99e
inflatus <inflatus@gmail.com>
2020-10-25 11:35:12 -0400

    updated scripts using pycodestyle

--- python/password_crack.py
+++ python/password_crack.py
@@ -6,13 +6,14 @@ from crypt import crypt
 from sys import argv
 from spwd import getspnam
 
+
 def main():
     uid = os.getuid()
     if (uid != 0):
-        print(Fore.YELLOW+"Use root!")
+        print(Fore.YELLOW + "Use root!")
         exit(1)
-    
-    if (len(argv)<=1):
+
+    if (len(argv) <= 1):
         print(Fore.YELLOW + "What user's password should we crack?" + Fore.RESET)
         username = input()
     else:
@@ -21,31 +22,30 @@ def main():
     print(Fore.CYAN + "Cracking your UNIX password for " + Fore.YELLOW + username)
 
     dict_file = open("10-million-password-list-top-1000000.txt")
-    encrypted_password=getspnam(username)[1]
+    encrypted_password = getspnam(username)[1]
 
     print(Fore.CYAN + "Encrypted password is " + Fore.YELLOW + encrypted_password)
-    
+
     count = 0
 
     for password in dict_file.readlines():
-        password=password.rstrip()
-        new_password=crypt(password,encrypted_password)
+        password = password.rstrip()
+        new_password = crypt(password, encrypted_password)
 
         print(Fore.YELLOW + "Trying password " + Fore.MAGENTA + password)
 
-        if (encrypted_password==new_password):
+        if (encrypted_password == new_password):
             print(Fore.GREEN + "Password found!")
-            print(Fore.YELLOW + "it took " + Fore.CYAN + str(count))
+            print(Fore.YELLOW + "It took " + Fore.CYAN + str(count))
             print(Fore.RESET + "The cracked password is " + Fore.BLUE + password)
             exit(0)
         else:
             print(Fore.RED + "Password failed")
             count += 1
-    
-    print(Fore.RED + "Better luck next time. Try another dictionary")
+
+    print(Fore.RED + "Better luck next time. Try another dictionary" + Fore.RESET)
     exit(1)
-    
+
 
 if __name__ == "__main__":
     main()
-
--- python/password_crack_no_colors.py
+++ python/password_crack_no_colors.py
@@ -5,13 +5,14 @@ from crypt import crypt
 from sys import argv
 from spwd import getspnam
 
+
 def main():
     uid = os.getuid()
     if (uid != 0):
         print("Use root!")
         exit(1)
-    
-    if (len(argv)<=1):
+
+    if (len(argv) <= 1):
         print("What user's password should we crack?")
         username = input()
     else:
@@ -20,19 +21,19 @@ def main():
     print("Cracking your UNIX password for " + username)
 
     dict_file = open("10-million-password-list-top-1000000.txt")
-    encrypted_password=getspnam(username)[1]
+    encrypted_password = getspnam(username)[1]
 
     print("Encrypted password is " + encrypted_password)
-    
+
     count = 0
 
     for password in dict_file.readlines():
-        password=password.rstrip()
-        new_password=crypt(password,encrypted_password)
+        password = password.rstrip()
+        new_password = crypt(password, encrypted_password)
 
         print("Trying password " + password)
 
-        if (encrypted_password==new_password):
+        if (encrypted_password == new_password):
             print("Password found!")
             print("It took " + str(count))
             print("The cracked password is " + password)
@@ -40,11 +41,10 @@ def main():
         else:
             print("Password failed")
             count += 1
-    
+
     print("Better luck next time. Try another dictionary")
     exit(1)
-    
+
 
 if __name__ == "__main__":
     main()
-
