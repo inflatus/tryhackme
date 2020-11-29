d024876c4ec58b44082c3f8408245050b17cbd72
inflatus <inflatus@gmail.com>
2020-11-29 10:59:38 -0500

    added a remainder script

---
+++ python/remainder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# used to find remainder
+
+print("What is the dividend?")
+q = input()
+print("What is the divisor?")
+r = input()
+
+remainder = (int(q) % int(r))
+print(remainder)
